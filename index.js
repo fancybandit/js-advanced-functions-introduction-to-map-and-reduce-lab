c4ebe901434734658ba062767cb891c03df21f90
Jacob Klimenko <droidedjake@gmail.com>
2021-07-25 18:31:31 -0400

    commit tree ebb75de823f9583b557e606328855270e054958d

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+// 'map()'-like functions
+
+function mapToNegativize(sourceArray) {
+    const newArray = [];
+    for (const val of sourceArray) {
+        newArray.push(-(val));
+    };
+    return newArray;
+};
+
+function mapToNoChange(sourceArray) {
+    const newArray = [];
+    for (const val of sourceArray) {
+        newArray.push(val);
+    };
+    return newArray;
+};
+
+function mapToDouble(sourceArray) {
+    const newArray = [];
+    for (const val of sourceArray) {
+        newArray.push(val * 2);
+    };
+    return newArray;
+};
+
+function mapToSquare(sourceArray) {
+    const newArray = [];
+    for (const val of sourceArray) {
+        newArray.push(val ** 2);
+    };
+    return newArray;
+};
+
+
+// 'reduce()'-like functions
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint;
+    for (const val of sourceArray) {
+        total += val;
+    };
+    return total;
+};
+
+function reduceToAllTrue(sourceArray) {
+    for (const val of sourceArray) {
+        if (!val) {return false;};
+    };
+    return true;
+};
+
+function reduceToAnyTrue(sourceArray) {
+    for (const val of sourceArray) {
+        if (val) {return true;};
+    };
+    return false;
+};
